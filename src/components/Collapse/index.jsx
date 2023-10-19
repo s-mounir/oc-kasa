f499d2be7ee6ff8b8acdf46c11ac1cc727361366
Sara Mounir <sara_mounir@hotmail.fr>
2023-10-19 19:15:30 +0200

    CSS About

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,6 +1,33 @@
 import { useState } from 'react'
+import styled from 'styled-components';
 
+const TitleDiv = styled.div`
+  background-color: #FF6060;
+  border-radius: 5px;
+  height: 52px;
+  display: flex;
+  align-items: center;
+  margin: 15px 100px;
+  padding: 0 30px;
+  justify-content: space-between;
+`
+const Title = styled.h2`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 142.6%;
+  color: #FFFFFF;
+`
 
+const Arrow = styled.button`
+  width: 32px;
+  height: 32px;
+  color: #FFFFFF;
+  background-color: #FF6060;
+  padding: 0;
+  border: none;
+`
 
 function Collapse(props){
   const [open, setOpen] = useState(false);
@@ -9,8 +36,17 @@ function Collapse(props){
   }
     return (
         <div>
-            <button onClick={toggle}>{props.label}</button>
-            {open && <div>{props.children}</div>}
+          <TitleDiv>
+            <Title>{props.label}</Title>
+            <Arrow onClick={toggle}>
+              {
+                open ? <svg xmlns="http://www.w3.org/2000/svg" height="32" fill="#FFF" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" height="32" fill="#FFF" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>
+              }
+              
+              
+            </Arrow>
+          </TitleDiv>
+          {open && <div>{props.children}</div>}
         </div>
     )
 }
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -9,22 +9,43 @@ const Body = styled.div`
   gap: 40px;
 `
 
+const TextDiv = styled.div`
+  background-color: #F7F7F7;
+  border-radius: 0px 0px 5px 5px;
+  margin: -20px 100px 15px 100px;
+  padding: 15px;
+`
+
+const Text = styled.p`
+  color: #FF6060;
+  margin: 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 142.6%;
+`
+
 function About() {
   return (
     <Body>
       <Banner page="About"/>
       <div>
         <Collapse label="Fiabilité">
-          <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
+          <TextDiv>
+            <Text>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</Text>
+          </TextDiv>
         </Collapse>
         <Collapse label="Respect">
-          <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+          <TextDiv>
+            <Text>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</Text>
+          </TextDiv>
         </Collapse>
         <Collapse label="Service">
-          <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+          <TextDiv><Text>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</Text></TextDiv>
         </Collapse>
         <Collapse label="Sécurité">
-          <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+          <TextDiv><Text>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</Text></TextDiv>
         </Collapse>
       </div>
     </Body>
